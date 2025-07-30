4d79ef58dfa23ce4951f384471b5a633aee3d8e2
VidhiPimple <vidhi.2403634201@vcet.edu.in>
2025-07-30 09:38:59 +0530

    Add files via upload

---
+++ ascii_hash.py
@@ -0,0 +1,14 @@
+def ascii_hash(input_string,size):
+    
+    total_ascii_sum = 0
+    for char in input_string:
+        ascii_value = ord(char)
+        total_ascii_sum += ascii_value
+
+    simple_hash = total_ascii_sum % size 
+    print(f"Sum of ascii values: {total_ascii_sum}")
+    print(f"Calculated hash: {simple_hash}")
+
+user_input = input("Enter a string: ")
+size = int(input("enter hash table size:"))
+ascii_hash(user_input,size)
